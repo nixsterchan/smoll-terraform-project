3dd55c86e98a8963288ef9b72987ebb4c4c5c2b8
nigelchan <nigelchan95@gmail.com>
2024-04-23 14:44:53 +0800

    Added in the variables for lambda scripts folder path, and the name of the lambda to be used for image resizing.

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "image_resize_lambda_name" {
+  type = string
+  description = "Name of the lambda function to be used for the image resizing."
+}
+
+variable "lambda_scripts_folder_name" {
+  type = string
+  description = "The folder path name where lambda functions exist in."
+}
