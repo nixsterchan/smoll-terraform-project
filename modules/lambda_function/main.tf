2e305f65df1aaa933cbc214674cf9721d4d227e9
nigelchan <nigelchan95@gmail.com>
2024-04-23 15:19:44 +0800

    Updated main.tf to have data archive block for the creation of the specified lambda function in zip format, and updated variables.tf accordingly.

--- modules/lambda_function/main.tf
+++ modules/lambda_function/main.tf
@@ -1,3 +1,11 @@
+# For retrieving a zipped lambda function module
+data "archive_file" "lambda_function_zip" {
+  type        = "zip"
+  source_dir  = var.lambda_function_code_path
+  output_path = "${path.module}/${var.function_name}_lambda_function.zip"
+}
+
+# Creates the base IAM role for the given lambda function name
 resource "aws_iam_role" "lambda_role" {
   name               = "${var.function_name}_lambda_role"
   description        = "IAM role creation for the ${var.function_name} lambda function." 
@@ -16,3 +24,17 @@ resource "aws_iam_role" "lambda_role" {
 }
 EOF
 }
+
+# For the addition of policies to the lambda IAM role
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "${var.function_name}-policy"
+  description = "IAM policy for the ${var.function_name} lambda function"
+  policy      = var.lambda_policy_document
+}
+
+# Does the attachment of a specified policy to a specific lambda IAM role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.id
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
--- modules/lambda_function/variables.tf
+++ modules/lambda_function/variables.tf
@@ -7,3 +7,8 @@ variable "lambda_policy_document" {
   type = string
   description = "IAM policy document for the created lambda function."
 }
+
+variable "lambda_function_code_path" {
+  type        = string
+  description = "Local path to the created lambda function's code directory"
+}
