583a7b401c19520b2202a473ee13da6f4ad17be6
nigelchan <nigelchan95@gmail.com>
2024-04-23 15:14:19 +0800

    Updated main.tf and variables.tf for the addition and attaching of a given policy document input to the created IAM role.

--- modules/lambda_function/variables.tf
+++ modules/lambda_function/variables.tf
@@ -2,3 +2,8 @@ variable "function_name" {
   type        = string
   description = "Name of the lambda function to be created."
 }
+
+variable "lambda_policy_document" {
+  type = string
+  description = "IAM policy document for the created lambda function."
+}
