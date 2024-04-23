d393d584781bb2f354d45abe3c01f72eeaf53612
nigelchan <nigelchan95@gmail.com>
2024-04-23 17:11:16 +0800

    Updated main.tf and outputs.tf for s3 module

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,4 +1,4 @@
 # Create your S3 bucket with the specified name
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -7,3 +7,8 @@ output "s3_bucket_id" {
   description = "Returns the id of the created S3 bucket."
   value       = aws_s3_bucket.s3_bucket.id
 }
+
+output "s3_bucket_name" {
+  description = "Returns the bucket name of the created S3 bucket."
+  value = aws_s3_bucket.s3_bucket.bucket
+}
