fc362178f56b5358c33616b9daf59402a74a50f5
nigelchan <nigelchan95@gmail.com>
2024-04-23 15:34:26 +0800

    Added in the aws lambda function creation block and aws cloudwatch log group for the lambda in main.tf of lambdas module. updated the variables accordingly

--- modules/lambda_function/main.tf
+++ modules/lambda_function/main.tf
@@ -38,3 +38,23 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
+# Creation of the lambda function and specifies the configs required
+resource "aws_lambda_function" "lambda_function" {
+  filename         = data.archive_file.lambda_function_zip.output_path
+  source_code_hash = filebase64sha256(data.archive_file.lambda_function_zip.output_path)
+  role             = aws_iam_role.lambda_role.arn
+  function_name    = var.function_name
+  runtime          = var.runtime
+  handler          = var.handler
+  architectures    = var.architectures
+  memory_size      = var.memory_size
+  timeout          = var.timeout
+  environment {
+    variables = var.environment_variables
+  }
+}
+
+# Creates a log group for this lambda to write out its logs to for use with CloudWatch Insights if necessary for debugging purposes
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
+}
--- modules/lambda_function/variables.tf
+++ modules/lambda_function/variables.tf
@@ -12,3 +12,34 @@ variable "lambda_function_code_path" {
   type        = string
   description = "Local path to the created lambda function's code directory"
 }
+
+variable "runtime" {
+  type        = string
+  description = "Runtime environment for the Lambda function (e.g., python3.11 or python3.12)"
+}
+
+variable "handler" {
+  type        = string
+  description = "Lambda function handler. (i.e. a typical one would be lambda_function.lambda_handler)"
+}
+
+variable "architectures" {
+  type = list(string)
+  description = "Specified architecture to be used for the creation of the lambda function. (e.g. ARM or x86)"
+}
+
+variable "timeout" {
+  type = number
+  description = "Specifies the time in seconds, that the Lambda will last before timing out. Max 15 mins"
+}
+
+variable "memory_size" {
+  type = number
+  description = "Specifies the amount of memory in MB that the Lambda function will run on with 10240MB as the maximum"
+}
+
+variable "environment_variables" {
+  type        = map(string)
+  description = "Environment variables for the Lambda function"
+  default     = {}
+}
